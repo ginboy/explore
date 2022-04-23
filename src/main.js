78e3c3f40e51745310e6bc88f9ebe4f673dc99b9
huangchuanbiao <1059459292@qq.com>
2022-04-23 18:42:58 +0800

    webpack+vue3基本的配置结束

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log("hello webpack");
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
+import store from "./store/index.js";
+import installVant from "@/plugins/vant.js";
+const app = createApp(App);
+installVant(app);
+app.use(router).use(store).mount("#app");
